63de3c60e8e3d9c55a879c60ccbc3dc77c590490
sadikhossain69 <sadikhossain433@gmail.com>
2022-05-22 22:44:22 +0600

    db connected

--- index.js
+++ index.js
@@ -3,12 +3,33 @@ const app = express()
 const cors = require('cors');
 const req = require('express/lib/request');
 const res = require('express/lib/response');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
 require('dotenv').config()
 
 // Middleware
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mjxci.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try{
+        await client.connect()
+        console.log("DB Connected"); 
+    }
+    finally{
+        // await client.close()
+    }
+}
+
+run().catch(console.dir)
+
+
 
 app.get('/', (req, res) => {
     res.send("Hello manufacture Website")
