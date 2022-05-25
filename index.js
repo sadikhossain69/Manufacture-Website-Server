424bc227a4b2b37c2883a1d00d67b623c63a356d
sadikhossain69 <sadikhossain433@gmail.com>
2022-05-25 21:11:57 +0600

    make admin api created

--- index.js
+++ index.js
@@ -137,6 +137,16 @@ async function run() {
             res.send(users)
         })
 
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updateDoc = {
+                $set: { role: 'admin' },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
     }
     finally {
         // await client.close()
